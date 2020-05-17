4904c88b403a7f946d9366c2e6f407c935c8c1f4
Okokoh Benjamin <ifihear@gmail.com>
2020-05-17 14:05:42 +0100

    community

--- assets_live__/js/process.js
+++ assets_live__/js/process.js
@@ -1,5 +1,46 @@
 
 
+
+$(".joinCommunityForm").on("submit", function(e) {
+    e.preventDefault();
+    $(this).LoadingOverlay("show");
+
+    var post_path = $(this).attr('action');
+    var contents = $(this).serialize();
+//
+    // alert(contents);
+    $.post(post_path,  contents, function(result){
+
+        $('.joinCommunityForm').LoadingOverlay("hide");
+
+        if(result['status'] === 'success'){
+            iziToast.success({
+                title: 'OK',
+                message: result['message'],
+                position: "topCenter"
+            });
+
+            location.reload();
+        }else{
+
+            iziToast.error({
+                title: 'Error',
+                message: result['message'],
+                position: "topCenter"
+            });
+
+        }
+
+    });
+
+});
+
+
+
+
+
+
+
 $(".businessProfileUpdateForm").on("submit", function(e) {
     e.preventDefault();
     $(this).LoadingOverlay("show");
@@ -442,6 +483,46 @@ $(".campaignCreateButton").on("click", function(e) {
 
 
 
+
+$(".CommunitylocationFilterForm").on("submit", function(e) {
+    e.preventDefault();
+    $(this).LoadingOverlay("show");
+
+    var post_path = $(this).attr('action');
+    var contents = $(this).serialize();
+//
+    $.post(post_path,  contents, function(result){
+        if(result['status'] === 'success'){
+            iziToast.success({
+                title: 'OK',
+                message: result['message'],
+                position: "topCenter"
+            });
+            // take him to the register page
+
+            window.location.href = $('#path_to_explore_community_business').val();
+
+        }else{
+
+            iziToast.error({
+                title: 'Error',
+                message: result['message'],
+                position: "topCenter"
+            });
+            $('.CommunitylocationFilterForm').LoadingOverlay("hide");
+
+        }
+
+    });
+
+});
+
+
+
+
+
+
+
 $(".locationFilterForm").on("submit", function(e) {
     e.preventDefault();
     $(this).LoadingOverlay("show");
@@ -588,3 +669,256 @@ if(my_answer==100) {
 
 
 
+
+
+
+
+$(".createCommunityForm").on("submit", function(e) {
+    e.preventDefault();
+    $(".createCommunityFormButton").LoadingOverlay("show");
+
+    var post_path = $(this).attr('action');
+    var contents = $(this).serialize();
+//
+    // alert(contents);
+    $.post(post_path,  contents, function(result){
+        if(result['status'] === 'success'){
+            iziToast.success({
+                title: 'OK',
+                message: result['message'],
+                position: "topCenter"
+            });
+            // take him to the register page
+            location.reload();
+
+        }else{
+
+            iziToast.error({
+                title: 'Error',
+                message: result['message'],
+                position: "topCenter"
+            });
+            $('.createCommunityFormButton').LoadingOverlay("hide");
+
+        }
+
+    });
+
+});
+
+
+
+
+
+/*
+
+$(".updateCommunityDetails").on("submit", function(e) {
+    e.preventDefault();
+    $(".updateCommunityDetailsButton").LoadingOverlay("show");
+
+    var post_path = $(this).attr('action');
+    var contents = $(this).serialize();
+//
+    // alert(contents);
+    $.post(post_path,  contents, function(result){
+        if(result['status'] === 'success'){
+            iziToast.success({
+                title: 'OK',
+                message: result['message'],
+                position: "topCenter"
+            });
+            // take him to the register page
+            location.reload();
+
+        }else{
+
+            iziToast.error({
+                title: 'Error',
+                message: result['message'],
+                position: "topCenter"
+            });
+            $('.updateCommunityDetailsButton').LoadingOverlay("hide");
+
+        }
+
+    });
+
+});
+*/
+
+
+
+
+
+
+$(".updateCommunityDetailsButton").on("click", function(e) {
+    e.preventDefault();
+    //tinyMCE.triggerSave();
+
+//    var action = $(this).data('action');
+
+    $('.updateCommunityDetailsButton').LoadingOverlay("show");
+
+    var post_path = $('.updateCommunityDetails').attr('action');
+    $(this).LoadingOverlay("show");
+    var fd = new FormData();
+    var files = $('#banner_file')[0].files[0];
+    fd.append('banner_file', files);
+
+    var contents = $('.updateCommunityDetails').serialize();
+
+    $.ajax({
+        url: post_path + '?'+contents,
+        type: 'post',
+        data: fd,
+        contentType: false,
+        processData: false,
+        success: function(result){
+            $('.updateCommunityDetailsButton').LoadingOverlay("hide");
+
+            if(result['status'] === 'success'){
+                iziToast.success({
+                    title: 'OK',
+                    message: result['message'],
+                    position: "topCenter"
+
+                });
+                location.reload();
+                $('.updateCommunityDetailsButton').LoadingOverlay("hide");
+
+            }else{
+                iziToast.error({
+                    title: 'Error',
+                    message: result['message'],
+                    position: "topCenter"
+
+                });
+                $('.updateCommunityDetailsButton').LoadingOverlay("hide");
+
+            }
+
+
+        },
+        error: function (e) {
+            //console.log("ERROR : ", e);
+            $('.updateCommunityDetailsButton').LoadingOverlay("hide");
+            iziToast.error({
+                title: 'Error',
+                message: "Something is wrong, make sure your image is not too large please, try again later..",
+                position: "topCenter"
+
+            });
+        }
+
+    });
+
+});
+
+
+
+
+
+
+
+$(".communityUploadLogoForm").on("submit", function(e) {
+    e.preventDefault();
+    $('.communityUploadLogoForm').LoadingOverlay("show");
+
+    var post_path = $('.communityUploadLogoForm').attr('action');
+    $(this).LoadingOverlay("show");
+    var fd = new FormData();
+    var files = $('#image_file')[0].files[0];
+    fd.append('image_file', files);
+
+    var contents = $('.communityUploadLogoForm').serialize();
+
+    $.ajax({
+        url: post_path + '?'+contents,
+        type: 'post',
+        data: fd,
+        contentType: false,
+        processData: false,
+        success: function(result){
+
+            if(result['status'] === 'success'){
+                iziToast.success({
+                    title: 'OK',
+                    message: result['message'],
+                    position: "topCenter"
+
+                });
+
+                location.reload();
+            }else{
+                iziToast.error({
+                    title: 'Error',
+                    message: result['message'],
+                    position: "topCenter"
+
+                });
+
+            }
+
+            $('.communityUploadLogoForm').LoadingOverlay("hide");
+
+        },
+        error: function (e) {
+            console.log("ERROR : ", e);
+            $('.communityUploadLogoForm').LoadingOverlay("hide");
+            iziToast.error({
+                title: 'Error',
+                message: "Something is wrong, make sure your image is not too large please, try again later..",
+                position: "topCenter"
+
+            });
+        }
+
+    });
+
+});
+
+
+
+
+
+
+
+$(".memberBusinessApproveButton").on("click", function(e) {
+
+    $(this).LoadingOverlay("show");
+    var business_id = $(this).data('business_id');
+    var action = $(this).data('action');
+    var member_id = $(this).data('member_id');
+
+    var post_path = $('#member_business_approve_path').val();
+    var community_id = $('#member_business_community_id').val();
+    var contents = {
+        business_id :  business_id,
+        community_id :  community_id,
+        member_id :  member_id,
+        status :  action,
+    };
+
+    $.post(post_path,  contents, function(result){
+        $('.memberBusinessApproveButton').LoadingOverlay("hide");
+        if(result['status'] === 'success'){
+            iziToast.success({
+                title: 'OK',
+                message: result['message'],
+                position: "topCenter"
+            });
+            $('.business_id_'+business_id).html(action);
+
+        }else{
+            iziToast.error({
+                title: 'Error',
+                message: result['message'],
+                position: "topCenter"
+            });
+            $('.memberBusinessApproveButton').LoadingOverlay("hide");
+
+        }
+
+    });
+
+});
